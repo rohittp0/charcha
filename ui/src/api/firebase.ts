44533314507ab0b9bb68aecd98130d0e92423670
Rohit T P <tprohit9@gmail.com>
2024-04-01 14:04:16 +0530

    added basic documentation

--- ui/src/api/firebase.ts
+++ ui/src/api/firebase.ts
@@ -3,16 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { collection, query, orderBy, startAfter, limit, getDocs, QueryConstraint, Timestamp } from "firebase/firestore";
+import firebaseConfig from "../../firebase-config";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAAe43qrBwZ_m-vynhx7ittRgiOHyhVRfk",
-    authDomain: "charchas.firebaseapp.com",
-    projectId: "charchas",
-    storageBucket: "charchas.appspot.com",
-    messagingSenderId: "959267204319",
-    appId: "1:959267204319:web:d91be37ba73131a7e2cd6a"
-};
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
