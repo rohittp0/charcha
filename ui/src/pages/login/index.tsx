eadcc2d9b483aab8aa7946ea27cd2ee7ffda0e7d
Rohit T P <tprohit9@gmail.com>
2024-04-01 18:46:45 +0530

    auto-forward login

--- ui/src/pages/login/index.tsx
+++ ui/src/pages/login/index.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth, googleProvider } from '../../api/firebase'; // Adjust the path as needed
 import { signInWithPopup} from "firebase/auth";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {useEffect, useState} from "react";
 
 const LoginPage = () => {
     const navigate = useNavigate(); // Hook to programmatically navigate
+    const [user, loading, error] = useAuthState(auth);
+    const [loginError, setLoginError] = useState<string>();
 
     const signInWithGoogle = () => {
         signInWithPopup(auth, googleProvider)
-            .then(({ user }) => {
-                console.log(user);
-                // Redirect to /chat after successful sign-in
-                navigate('/chat');
-            }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error('Error during sign-in with Google:', errorCode, errorMessage);
-        });
+            .then(({ user }) => user && navigate('/chat'))
+            .catch((error) => setLoginError(error.message));
     };
+
+    useEffect(() => {
+        if (user) {
+            navigate('/chat');
+        }
+
+        if (error) {
+            setLoginError(error.message);
+        }
+    }, [user, navigate, error]);
+
     return (
         <section className="h-screen"
                  style={{
@@ -46,8 +52,12 @@ const LoginPage = () => {
                                 Click the button below to sign in with your Google account. We won't take any of your
                                 personal information.
                             </p>
+                            {loginError && (
+                                <div className="text-sm text-red-600 dark:text-red-400">{loginError}</div>
+                            )}
                             <button
                                 type="button"
+                                disabled={loading}
                                 onClick={signInWithGoogle}
                                 className="flex items-center justify-center w-full text-black bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 font-medium text-sm px-5 py-2.5 text-center dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-700"
                             >
